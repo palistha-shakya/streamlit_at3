41503d92f7027380c5756dc5a4d1acb9bba4a505
Adi0399 <Aditya.M.Kulkarni@student.uts.edu.au>
2025-10-30 08:43:51 +1100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,13 +4,18 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from students.ethereum import run_eth_tab
+from students.xrp import run_xrp_tab
 
 st.set_page_config(page_title="Crypto Forecast Dashboard", layout="wide")
+
 
 st.title("Crypto Forecast Dashboard")
 st.write("Access real-time data and next-day price predictions for Ethereum.")
 
 # single tab for Ethereum
-tab1 = st.tabs(["Ethereum"])[0]
-with tab1:
+tab_eth, tab_xrp = st.tabs(["Ethereum", "XRP"])
+with tab_eth:
     run_eth_tab()
+
+with tab_xrp:
+    run_xrp_tab()
